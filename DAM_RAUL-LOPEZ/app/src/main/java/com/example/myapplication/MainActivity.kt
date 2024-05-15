00687790481c587e336e2b0bc01540a339959160
RAUL ANDERSSON LOPEZ PURIS <20a3110155@untels.edu.pe>
2024-05-15 11:11:33 -0500

    3er commit

--- DAM_RAUL-LOPEZ/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ DAM_RAUL-LOPEZ/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn1 = findViewById<Button>(R.id.button)
+        btn1.setOnClickListener { view: View? ->
+            Toast.makeText(
+                this,
+                "Se envió sus datos",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
